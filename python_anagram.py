37a806cc0fba0c4ef357668388b1f0112defb6b2
bradserlis <bradserlis@yahoo.com>
2018-05-23 15:15:25 -0700

    first commit

---
+++ python_anagram.py
@@ -0,0 +1,15 @@
+# def anagram_finder(context):
+# 	context.split('')
+
+
+def anagram_finder(word1, word2):
+  newlist1 = list(word1)
+  newlist2 = list(word2)
+  newlist1.sort()
+  newlist2.sort()
+  for element in range(0, len(newlist2)):
+    if newlist1[element] != newlist2[element]:
+      return False
+  return True
+
+print(anagram_finder('yoshi', 'syoih'))
