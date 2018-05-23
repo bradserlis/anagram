cba849c970605a7ac244294d36cdb5b88cca538a
bradserlis <bradserlis@yahoo.com>
2018-05-23 16:19:53 -0700

    cleanup!

--- python_anagram.py
+++ python_anagram.py
@@ -3,8 +3,8 @@
 
 
 def anagram_finder(word1, word2):
-  newlist1 = list(word1)
-  newlist2 = list(word2)
+  newlist1 = list(word1.lower())
+  newlist2 = list(word2.lower())
   newlist1.sort()
   newlist2.sort()
   for element in range(0, len(newlist2)):
